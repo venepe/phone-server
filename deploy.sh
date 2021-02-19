591568b697b66c65c49ab4caebe5408afaf1c0f5
venepe <venepe@users.noreply.github.com>
2021-02-19 02:10:15 -0600

    Update deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 source ~/.bashrc
 gcloud builds submit --tag gcr.io/bubblepop/api-nodejs
-gcloud run deploy api-nodejs --image gcr.io/bubblepop/api-nodejs --add-cloudsql-instances bubblepop:us-central1:bubblepop-db-production   --update-env-vars INSTANCE_CONNECTION_NAME="bubblepop:us-central1:bubblepop-db-production" --platform managed --allow-unauthenticated
+gcloud run deploy api-nodejs --image gcr.io/bubblepop/api-nodejs --add-cloudsql-instances bubblepop:us-central1:bubblepop-db-production   --update-env-vars INSTANCE_CONNECTION_NAME="bubblepop:us-central1:bubblepop-db-production",GOOGLE_APPLICATION_CREDENTIALS="./secrets/firebase-adminsdk.json" --platform managed --allow-unauthenticated
